ae3eabeaed6e96f64ab6e0c12afa86edb3212fa9
claudio-py <101283035+claudio-py@users.noreply.github.com>
2023-06-17 20:14:19 -0300

    code restructuration

--- script.js
+++ script.js
@@ -2,7 +2,31 @@ const swiper = new Swiper('.swiper', {
 	// Optional parameters
 	direction: 'horizontal',
 	loop: true,
-
+	  breakpoints: {
+			  280: {
+      slidesPerView: 1,
+      
+    },
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 4,
+      spaceBetween: 40
+    },
+		  720: {
+      slidesPerView: 5,
+      spaceBetween: 40
+    }
+  },
 	// If we need pagination
 	pagination: {
 		el: '.swiper-pagination'
@@ -18,4 +42,20 @@ const swiper = new Swiper('.swiper', {
 	scrollbar: {
 		el: '.swiper-scrollbar'
 	}
+})
+
+const imgElements = document.querySelectorAll('.item')
+const imgElements2 = document.querySelectorAll('.item-description')
+
+imgElements.forEach(function (imgElement) {
+	imgElement.addEventListener('click', function () {
+		this.classList.toggle('active')
+
+	})
+})
+
+imgElements2.forEach(function (imgElement) {
+	imgElement.addEventListener('click', function () {
+		this.classList.toggle('active2')
+	})
 })
